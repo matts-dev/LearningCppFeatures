cfab9cde38aae8dc528934028f9c4955d7c0cf4a
dev-user <no.email>
2017-05-09 09:59:59 -0400

    Created JakesQuestionOnFields.h

---
+++ LearningCpp/LearningCpp/JakesQuestionOnFields.h
@@ -0,0 +1,42 @@
+#pragma once
+#include<iostream>
+
+//To answer the question: will the field be created twice without an initializer list 
+//and setting it to a value in constructor body? 
+//Yes it appears to create two objects! (unless you use an intializer list!)
+class PrintOnCtor {
+public:
+	//print something on ctor
+	PrintOnCtor() {
+		std::cout << "ctor!" << std::endl;
+	}
+	//print something on dtor
+	~PrintOnCtor() {
+		std::cout << "dtor!" << std::endl;
+	}
+
+};
+
+
+class HasFieldThatPrints{
+	bool value = true;
+	PrintOnCtor willIprintTwice_Qmark;
+public:
+	//prints:
+	//arg ctor!
+	//arg ctor!
+	//dtor!
+	//dtor!
+	HasFieldThatPrints() {
+		//no-arg ctor called before constructor entered
+		//no-arg ctor called on assignment
+		this->willIprintTwice_Qmark = PrintOnCtor();
+	}
+
+	//prints:
+	//ctor!
+	//dtor!
+	HasFieldThatPrints(bool arg) : willIprintTwice_Qmark() {
+	}
+	~HasFieldThatPrints() {}
+};
--- LearningCpp/LearningCpp/sketch.cpp
+++ LearningCpp/LearningCpp/sketch.cpp
@@ -9,13 +9,14 @@
 #include"verboseclasses.h"
 #include"DummyClass.h"
 #include"inhert.hpp"
+#include"JakesQuestionOnFields.h"
 #include <cstdint>
 #include <climits>
+#include <bitset>
 
 using namespace std;
 using sptrs::Base;
 
-void printBytes(char* start, int numBytes);
 
 //This is how you foward declare functions in namespaces
 namespace polyTest {
@@ -24,14 +25,63 @@ namespace polyTest {
 	//added something to test diff on commits
 }
 
+
+void internetCheckSum();
+void crc();
+
 void sketch() {
-	polyTest::testPolyCompare();
+
+	//internetCheckSum();
+	{
+		cout << "\n\nnoarg" << endl;
+		HasFieldThatPrints y;
+	}
+
+	{
+		cout << "\n\narg" << endl;
+		HasFieldThatPrints x(true);
+	}
 }
 
-void printBytes(char* ptr, int numBytes) {
-	for (int i = 0; i < numBytes; ++i, ++ptr) {
-		printf("%.2x ", *ptr);
-	} 
-	printf("\n");
+void internetCheckSum() {
+	string data = "Link Layer";
+
+	int sum = 0;
+	for (unsigned int i = 0; i < data.length(); i += 2) {
+		if (i + 1 < data.length()) {
+			sum += ((data[i] << 8) + data[i + 1]);
+		}
+		else {
+			sum += data[i] << 8;
+		}
+	}
+
+	int mask = ~(-1 << 16);
+	int sigBits = sum >> 16;
+
+	//add over the carry 
+	while (sigBits > 0) {
+
+		cout << bitset<16>(sum) << endl;
+		int carry = sum >> 16;
+
+		//clear off what was carried
+		sum &= mask;
+
+		// add back what was carried 
+		sum += carry;
+
+		sigBits = sum >> 16;
+
+	}
+
+	sum = ~sum & mask;
+
+	cout << bitset<16>(sum) << endl;
+}
+
+void crc()
+{
+
 }
 
